b7c429e6b9b3bd4d00e7ae808af2cd9402f8c7a5
Hankscr459 <defcw58@gmail.com>
2021-03-08 17:04:37 +0800

    Config File & Uploader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,13 @@
 <?php
+    // Load Config
+    require_once 'config/config.php';
+
     // Load Liberies
     require_once 'libraries/Core.php';
     require_once 'libraries/Controller.php';
     require_once 'libraries/Database.php';
+
+    // Autoload Core Libraries
+    spl_autoload_register(function($className) {
+        require_once 'libraries/' . $className . '.php';
+    });
